1ff71002e19a3925ac38b681d23646a9481e34bb
Randy Ramli <ramli.r@northeastern.edu>
2021-11-09 00:25:20 -0800

    fix encoder

--- Stegonography.py
+++ Stegonography.py
@@ -1,39 +1,67 @@
+import cv2
 import numpy as np
-import PIL as pic
+
+def binaryConverter(message):
+    if type(message) == str:
+        return ''.join([format(ord(i), '08b') for i in message])
+    elif type(message) == np.ndarray or type(message) == bytes:
+        return [format(i, '08b') for i in message]
+    elif type(message) == int:
+        return format(message, "08b")
 
 def Encoder():
-    image = input("Please enter the image name with the extension: \n")
-    img = pic.open(image, 'r')
-    img.show()
+    pic_input = input("Please enter the image name with the extension: \n")
+    pic_open = cv2.imread(pic_input)
+    print(type(pic_open))
 
 
     text = input("Please input the message you want to encrypt: \n")
     if len(text) == 0:
         return ValueError("Message is empty")
     
-    encodedImage = pixelManipulate(img, text)
+    image_copy = pic_open.copy()
+    
+    encodedImage = leastSigByte(image_copy, text)
+
+    newimg = input("Enter the name of the new image with extension: ")
+    cv2.imwrite(newimg, encodedImage)
+    
 
 def Decoder():
     pass
 
-def pixelManipulate(image,message):
+def leastSigByte(image,message):
     
-    img_Size = len(image.fp.read())
+    max_bytes = image.shape[0] * image.shape[1] * 3 //8
 
-    if len(message) > img_Size:
+    if len(message) > max_bytes:
         raise ValueError("The Message is too big for the picture")
+
+    
+    message += "##"
     
     message_binary = binaryConverter(message)
 
     message_len = len(message_binary)
 
-def binaryConverter(message):
-    if type(message) == str:
-        return ''.join([format(ord(i), '08b') for i in message])
-    elif type(message) == bytes or type(message) == np.ndarray:
-        return [format (i, '08b') for i in message]
-    elif type(message) == int or type(message) == np.uint8:
-        return format(message, '08b')
+    index = 0
+
+    for data in image:
+        for pixel in data:
+            r,g,b = binaryConverter(pixel)
+            if index < message_len:
+                pixel[0] = int(r[:-1] + message_binary[index], 2)
+                index += 1
+            if index < message_len:
+                pixel[1] = int(g[:-1] + message_binary[index], 2)
+                index += 1
+            if index < message_len:
+                pixel[2] = int(b[:-1] + message_binary[index], 2)
+                index += 1
+            if index >= message_len:
+                break
+    return image
+
 
 def main():
     print("Welcome to Stegonoraphy App")
